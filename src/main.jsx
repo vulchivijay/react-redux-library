3dca7e93f0dab2371a66188a79ddc09f909dd9ee
Vijayakumar, Vulchi (External) <Vulchi.Vijayakumar@xerox.com>
2025-10-01 19:53:59 +0530

    typo correction for build failure in vercile

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx';
 
 import { Provider } from 'react-redux';
-import store from './store/index.jsx';
+import store from './store/Index';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
